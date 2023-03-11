4f43a07f5565a9e9542bc1746ec1cd7203bffc40
Imran Mamin <imran.mamin@aalto.fi>
2023-03-11 23:24:23 +0200

    Add menubar and 'Start Again'-button to the GUI

--- src/main/scala/GUI/Main.scala
+++ src/main/scala/GUI/Main.scala
@@ -3,6 +3,9 @@ package GUI
 import scalafx.application.JFXApp3
 import scalafx.scene.Scene
 import scalafx.scene.layout.Pane
+import scalafx.scene.control.{Label, MenuBar, MenuItem, Menu, SeparatorMenuItem}
+import scalafx.scene.control.Button
+import scalafx.Includes._
 
 object Main extends JFXApp3:
 
@@ -11,5 +14,27 @@ object Main extends JFXApp3:
       title = "Killer-Sudoku"
       width = 800
       height = 600
+      scene = new Scene(800, 600) {
+        // Menu
+        val menuBar = new MenuBar
+        val fileMenu = new Menu("File")
+        val openItem = new MenuItem("Open file")
+        openItem.onAction = (event) => println("Open file -button in the menubar is clicked")
+        val saveItem = new MenuItem("Save file")
+        saveItem.onAction = (event) => println("Save file -button in the menubar is clicked")
+
+        fileMenu.items = List(openItem, saveItem)
+        menuBar.menus = List(fileMenu)
+
+        // Start Again -button
+        val startAgainButton = Button("Start Again")
+        startAgainButton.onAction = (event) => println("Clicked Start again -button")
+        startAgainButton.layoutX = 500
+        startAgainButton.layoutY = 0
+
+        // Adding menu and Start Again -button to the content
+        content = List(menuBar, startAgainButton)
+      }
+
 
 end Main
