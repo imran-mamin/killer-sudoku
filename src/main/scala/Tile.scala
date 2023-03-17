5e05c04834128f8f9d7040077df6ef34e35953f3
Imran Mamin <imran.mamin@aalto.fi>
2023-03-17 20:57:30 +0200

    remove subarea-reference from the Tile-class

--- src/main/scala/Tile.scala
+++ src/main/scala/Tile.scala
@@ -3,10 +3,12 @@ package scala
 import scala.collection.mutable.Buffer
 import java.awt.Color
 
-class Tile(column: Int, row: Int, square: Int, subArea: Subarea):
+class Tile(column: Int, row: Int, square: Int):
   val candidates: Buffer[Int] = Buffer()
   var neighbors: Vector[Tile] = Vector()
   var currentNumber: Option[Int] = None
+  
+  // These could be redundant TODO: Check variables below.
   var color: Option[Color] = None
   var targetSum: Option[Int] = None
 
