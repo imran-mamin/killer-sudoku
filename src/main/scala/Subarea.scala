cfc2fb356a267f9b21ea6138f769f9334918b5d5
Imran Mamin <imran.mamin@aalto.fi>
2023-03-18 14:32:00 +0200

    add method 'showPossibleCombinations' to the Subarea class, but not implement it yet

--- src/main/scala/Subarea.scala
+++ src/main/scala/Subarea.scala
@@ -6,6 +6,13 @@ import scala.collection.mutable.Buffer
 class Subarea(targetSum: Int, tiles: Vector[Tile], tileWithTargetSum: Tile):
   var neighbors: Vector[Subarea] = Vector()
   var color: Option[Color] = None
+
+  def showPossibleCombinations(): List[String] =
+    // var combinations: Buffer[String] = Buffer()
+    ???
+
+
+  // These may be redundant TODO: Check this out later, when implementing graph-coloring
   val preSubarea: Buffer[Subarea] = Buffer()
   val postSubarea: Buffer[Subarea] = Buffer()
   
